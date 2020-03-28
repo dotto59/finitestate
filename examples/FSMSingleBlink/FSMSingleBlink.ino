d78137bb3e73de820b442ec5288262f94188e89e
dotto59 <alex.palmese@gmail.com>
2020-03-28 17:42:03 +0100

    Updated 2.0 examples

--- examples/FSMSingleBlink/FSMSingleBlink.ino
+++ examples/FSMSingleBlink/FSMSingleBlink.ino
@@ -1,18 +1,17 @@
 /*
-@file FSMBlink.ino
+@file FSMSingleBlink.ino
 @version 2.0
 @author Alex Palmese
 @contact alex.palmese@gmail.com
 @description
-"BLINK" FINITE STATE MACHINE SAMPLE
-This basic Finite State Machine will simply flash the 
-builtin LED for a fixed time.
+"SINGLE BLINK" FINITE STATE MACHINE SAMPLE
+This basic Finite State Machine will simply flash the builtin LED for 
+a fixed time when the button is pressed.
+Connect a button to pin 7, with a pulldown resistor (1k+).
 
 It has only two states: 
-* START turns the LED on and pauses for the amount of time 
-contained in the parameter, then switches to LED state
-* LED turns the LED off and pauses for the amount of time 
-contained in the parameter, then switches back to START 
+* START turns the LED off and when the button is pressed, turns the led ON and switches to LED state
+* LED turns the LED on and after the amount of time contained in the parameter, switches back to START 
 */
 #include <FiniteState.h>
 
@@ -24,7 +23,8 @@ FiniteState fsm;
 #define S_START 1
 #define S_LED 2
 // Conditions
-#define C_TIMER 0
+#define C_BUTTON 1
+#define C_TIMER 2
 // Actions
 #define A_LED_OFF 0
 #define A_LED_ON 1
@@ -33,25 +33,25 @@ FiniteState fsm;
 // SYMBOLS
 // ***********************************************
 #define LED 13
+#define BUTTON 7
 
 // ***********************************************
 // GLOBALS
 // ***********************************************
 long millis0;
+bool btnPressed = false;
+int prevState = 0;
 
 // ***********************************************
 void setup() {
   Serial.begin(115200);
-
   pinMode(LED, OUTPUT);
 
   // State definitions
-  fsm.Write(S_START, C_TIMER, A_LED_ON, 1000, S_LED);
+  fsm.Write(S_START, C_BUTTON, A_LED_ON, 0, S_LED);
   fsm.Write(S_LED, C_TIMER, A_LED_OFF, 1000, S_START);
-
   // Set the callback functions
   fsm.SetFunctions(&TestCondition, &DoAction);
-
   // Set the machine to first state
   fsm.Set(S_START);
 }
@@ -71,6 +71,8 @@ boolean TestCondition(int condition) {
       return true;
     }
     return false;
+  case C_BUTTON:
+    return ( digitalRead(BUTTON) == HIGH );
   case C_ELSE:
     return true;
   }
